a41a4906b4d30f234b24df94c78e36c45eef9012
haoninghsu <haoninghsu0203@gmail.com>
2020-06-10 01:39:53 +0800

    added reference in about page

---
+++ about/main.js
@@ -0,0 +1,12 @@
+/* function historySlider() {
+    //History Images slide
+    var historyimages = jQuery('#history-images');
+    if (historyimages.length > 0) {
+        historyimages.owlCarousel({
+            singleItem: true,
+            pagination: false,
+            autoPlay: 2000,
+            slideSpeed: 300
+        });
+    }
+}; */
