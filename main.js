3215042bc205921c2d402a0b6c32820b944890f6
haoninghsu <haoninghsu0203@gmail.com>
2020-06-11 09:15:43 +0800

    Added filter

--- main.js
+++ main.js
@@ -1,94 +1,112 @@
 var arr = [
   [
     `samoyed`,
+    "white",
     `<a class="card" href="https://www.instagram.com/mayapolarbear/">
-<div class="recommend-img" style="background-image:url(image/samoyed.jpg);">
-</div>
-<div class="recommend-content">
-  <h3 class = "recommend-name">Samoyed</h3>
-  <p class = "recommend-descr">The land cloud</p>
-</div>
- 
-<div class = "insta">
-  <img class="ins-icon" src="image/instagram-sketched.svg">
-  <h6 class = "ins"> Recommended Instagram account </h6>
-</div>           
-</a>`,
+  <div class="recommend-img" style="background-image:url(image/samoyed.jpg);">
+  </div>
+  <div class="recommend-content">
+    <h3 class = "recommend-name">Samoyed</h3>
+    <p class = "recommend-descr">The land cloud</p>
+  </div>
+   
+  <div class = "insta">
+    <img class="ins-icon" src="image/instagram-sketched.svg">
+    <h6 class = "ins"> Recommended Instagram account </h6>
+  </div>           
+  </a>`,
+    "five",
+    "frigid",
   ],
   [
     `red panda`,
+    "red",
     `<a class="card" href="https://www.instagram.com/daniele.tokyo/">
-<div class="recommend-img" style="background-image:url(image/redpanda-unsplash.jpg);">
-</div>
-<div class="recommend-content">
-  <h3 class = "recommend-name">Red Panda</h3>
-  <p class = "recommend-descr">Not a racoon</p>
-</div>
-<div class = "insta">
-<img class="ins-icon" src="image/instagram-sketched.svg">
-<h6 class = "ins"> Recommended Instagram account </h6>
-</div> 
-</a>`,
+  <div class="recommend-img" style="background-image:url(image/redpanda-unsplash.jpg);">
+  </div>
+  <div class="recommend-content">
+    <h3 class = "recommend-name">Red Panda</h3>
+    <p class = "recommend-descr">Not a racoon</p>
+  </div>
+  <div class = "insta">
+  <img class="ins-icon" src="image/instagram-sketched.svg">
+  <h6 class = "ins"> Recommended Instagram account </h6>
+  </div> 
+  </a>`,
+    "three",
+    "temperate",
   ],
   [
     `quokka`,
+    "brown",
     `<a class="card" href="https://www.instagram.com/quokkadailyshow/">
-<div class="recommend-img" style="background-image:url(image/quokka.jpg);">
-</div>
-<div class="recommend-content">
-  <h3 class = "recommend-name">Quokka</h3>
-  <p class = "recommend-descr">The happiest animal on the world</p>
-</div>
-<div class = "insta">
-<img class="ins-icon" src="image/instagram-sketched.svg">
-<h6 class = "ins"> Recommended Instagram account </h6>
-</div> 
-</a>`,
+  <div class="recommend-img" style="background-image:url(image/quokka.jpg);">
+  </div>
+  <div class="recommend-content">
+    <h3 class = "recommend-name">Quokka</h3>
+    <p class = "recommend-descr">The happiest animal on the world</p>
+  </div>
+  <div class = "insta">
+  <img class="ins-icon" src="image/instagram-sketched.svg">
+  <h6 class = "ins"> Recommended Instagram account </h6>
+  </div> 
+  </a>`,
+    "three",
+    "temperate",
   ],
   [
     `sea Otter`,
+    "brown",
     `<a class="card" href="https://www.instagram.com/vanaqua/">
-<div class="recommend-img" style="background-image:url(image/sea-otter-2.jpg);">
-</div>
-<div class="recommend-content">
-  <h3 class = "recommend-name">Sea Otter</h3>
-  <p class = "recommend-descr">Not so fluffy but still adorable</p>
-</div>
-<div class = "insta">
-<img class="ins-icon" src="image/instagram-sketched.svg">
-<h6 class = "ins"> Recommended Instagram account </h6>
-</div> 
-</a>`,
+  <div class="recommend-img" style="background-image:url(image/sea-otter-2.jpg);">
+  </div>
+  <div class="recommend-content">
+    <h3 class = "recommend-name">Sea Otter</h3>
+    <p class = "recommend-descr">Not so fluffy but still adorable</p>
+  </div>
+  <div class = "insta">
+  <img class="ins-icon" src="image/instagram-sketched.svg">
+  <h6 class = "ins"> Recommended Instagram account </h6>
+  </div> 
+  </a>`,
+    "one",
+    "temperate",
   ],
   [
     `capybara`,
+    "brown",
     `<a class="card" href="https://www.instagram.com/tori_tori_capy/">
-<div class="recommend-img" style="background-image:url(image/Capybara.jpg);">
-</div>
-<div class="recommend-content">
-  <h3 class = "recommend-name">Capybara</h3>
-  <p class = "recommend-descr">The largest rodent who loves hot spring</p>
-</div>
-<div class = "insta">
-<img class="ins-icon" src="image/instagram-sketched.svg">
-<h6 class = "ins"> Recommended Instagram account </h6>
-</div> 
-</a>`,
+  <div class="recommend-img" style="background-image:url(image/Capybara.jpg);">
+  </div>
+  <div class="recommend-content">
+    <h3 class = "recommend-name">Capybara</h3>
+    <p class = "recommend-descr">The largest rodent who loves hot spring</p>
+  </div>
+  <div class = "insta">
+  <img class="ins-icon" src="image/instagram-sketched.svg">
+  <h6 class = "ins"> Recommended Instagram account </h6>
+  </div> 
+  </a>`,
+    "three",
+    "tropical",
   ],
   [
     `Malamute`,
+    "black",
     `<a class="card" href="https://www.instagram.com/dubs_uw/">
-<div class="recommend-img" style="background-image:url(image/Malamute.jpg);">
-</div>
-<div class="recommend-content">
-  <h3 class = "recommend-name">Malamute</h3>
-  <p class = "recommend-descr">We love Dubs. We love smiling malamutes.</p>
-</div>
-<div class = "insta">
-<img class="ins-icon" src="image/instagram-sketched.svg">
-<h6 class = "ins"> Recommended Instagram account </h6>
-</div> 
-</a>`,
+  <div class="recommend-img" style="background-image:url(image/Malamute.jpg);">
+  </div>
+  <div class="recommend-content">
+    <h3 class = "recommend-name">Malamute</h3>
+    <p class = "recommend-descr">We love Dubs. We love smiling malamutes.</p>
+  </div>
+  <div class = "insta">
+  <img class="ins-icon" src="image/instagram-sketched.svg">
+  <h6 class = "ins"> Recommended Instagram account </h6>
+  </div> 
+  </a>`,
+    "three",
+    "frigid",
   ],
 ];
 
@@ -105,11 +123,14 @@ function buildAllCards(currentArr) {
   let div = document.getElementById("cards");
   div.innerHTML = "";
   for (i = 0; i < arr.length; i++) {
-    buildOneCard(currentArr[i][1], div);
+    buildOneCard(currentArr[i][2], div);
   }
 }
 
 var searchInput = document.getElementById("animal-filter");
+var colorDropDown = document.getElementById("color-select");
+var fluffDropDown = document.getElementById("fluff-select");
+var habitatDropDown = document.getElementById("habitat-select");
 //compare the input with the animal list
 function isAnimalFound(animal) {
   //Get the input
@@ -123,6 +144,33 @@ function isAnimalFound(animal) {
   }
 }
 
+function isAnimalColor(animal) {
+  var selectedColor = colorDropDown.value;
+  if (animal[1] === selectedColor) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isAnimalFluff(animal) {
+  var selectedFluff = fluffDropDown.value;
+  if (animal[3] === selectedFluff) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isAnimalHabitat(animal) {
+  var selectedColor = habitatDropDown.value;
+  if (animal[4] === selectedColor) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 //listen when user types in the filed
 searchInput.addEventListener("input", function () {
   //filter animals
@@ -130,5 +178,20 @@ searchInput.addEventListener("input", function () {
   /*console.log(arr); */
   console.log(filtered_animals);
   //Update and display the list
+  buildAllCards(filtered_animals);
+});
+
+colorDropDown.addEventListener("change", function () {
+  var filtered_animals = arr.filter(isAnimalColor);
+  buildAllCards(filtered_animals);
+});
+
+fluffDropDown.addEventListener("change", function () {
+  var filtered_animals = arr.filter(isAnimalFluff);
+  buildAllCards(filtered_animals);
+});
+
+habitatDropDown.addEventListener("change", function () {
+  var filtered_animals = arr.filter(isAnimalHabitat);
   buildAllCards(filtered_animals);
 });
